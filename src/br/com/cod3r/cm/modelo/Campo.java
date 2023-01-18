c5e6ad9348b13069bb363530e5a1f5275713202d
marceloleal <marcelosleal@outlook.com>
2023-01-18 18:24:25 -0300

    Implementando funções para o padrão Observer

--- src/br/com/cod3r/cm/modelo/Campo.java
+++ src/br/com/cod3r/cm/modelo/Campo.java
@@ -13,10 +13,20 @@ public class Campo {
     private boolean marcado = false;
 
     private List<Campo> vizinhos = new ArrayList<>();
+    private List<CampoObservador> observadores = new ArrayList<>();
 
     public Campo(int linha, int coluna) {
         this.linha = linha;
         this.coluna = coluna;
+    }
+
+    public void registraObservador(CampoObservador observador) {
+        observadores.add(observador);
+    }
+
+    private void notificarObservadores(CampoEvento evento) {
+        observadores.stream()
+                .forEach(o -> o.eventoOcorreu(this, evento));
     }
 
     public boolean adicionarVizinho(Campo vizinho) {
@@ -42,17 +52,25 @@ public class Campo {
     public void alternarMarcacao() {
         if (!aberto) {
             marcado = !marcado;
+
+            if (marcado) {
+                notificarObservadores(CampoEvento.MARCAR);
+            } else {
+                notificarObservadores(CampoEvento.DESMARCAR);
+            }
         }
     }
 
     public boolean abrir() {
 
         if (!aberto && !marcado) {
-            aberto = true;
 
             if (minado) {
-                // TODO Implementar nova versão
+                notificarObservadores(CampoEvento.EXPLODIR);
+                return true;
             }
+
+            setAberto(true);
 
             if (vizinhancaSegura()) {
                 vizinhos.forEach(v -> v.abrir());
@@ -82,6 +100,10 @@ public class Campo {
 
     public void setAberto(boolean aberto) {
         this.aberto = aberto;
+
+        if (aberto) {
+            notificarObservadores(CampoEvento.ABRIR);
+        }
     }
 
     public boolean isAberto() {
