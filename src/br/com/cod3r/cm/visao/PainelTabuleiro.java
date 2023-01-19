0701333a76a221cd9715ce9cfe0c8f33587bd3bc
marceloleal <marcelosleal@outlook.com>
2023-01-19 10:22:48 -0300

    Inicio implementação do ambiente gráfico

---
+++ src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,23 @@
+package br.com.cod3r.cm.visao;
+
+import br.com.cod3r.cm.modelo.Campo;
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PainelTabuleiro extends JPanel {
+
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+        setLayout(new GridLayout(
+                tabuleiro.getLinhas(), tabuleiro.getColunas()));
+
+        tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+        tabuleiro.registrarObservador(e -> {
+            // TODO mostrar resultado pra o usuário!
+        });
+
+    }
+
+}
